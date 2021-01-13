3087773022eb9b0564d6fd5864b160b92e013651
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-13 14:24:10 -0400

    adicionando coaches via vuex
    en  esta clase se colocan algunos comentarios en el archivo mutations.js

--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -1,4 +1,5 @@
 export default {
+    // este es el componente que se ejecuta al ser llamado desde el exterior, actions es solo como un puente entre ambos.
     registerCoach(state, payload) {
         state.coaches.push(payload);
     }
