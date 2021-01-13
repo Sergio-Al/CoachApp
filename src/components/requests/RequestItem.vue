4214fcd9ad1538ecf969163c8c39539b4912f21b
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-13 19:30:03 -0400

    Mostrando request de llegada
    creamos la estructura para mostrar los requests de los usuarios que buscan un coach
    -adicionamos estructura en requestsItem.vue creando su carpeta request en componentes
    -llamamos en requests recieved los items que tiene.
    -creamos mas logica en getters e index de la carpeta 'store/modules/requests'
    -modificamos index de la carpeta 'store' para adicionar getters

---
+++ src/components/requests/RequestItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <li>
+        <div>
+            <a :href="emailLink">{{ email }}</a>
+        </div>
+        <p>{{ message }}</p>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['email', 'message'],
+    computed: {
+        emailLink() {
+            // 'mailto' es una instruccion especial que cada browser lo entendera.
+            // especial para emails, el navegador abrira automaticamente el link del local email
+            return 'mailto:' + this.email;
+        }
+    }
+}
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+a {
+  color: #3d008d;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover,
+a:active {
+  color: #8d007a;
+}
+
+p {
+  margin: 0.5rem 0 0 0;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,7 @@ const store = createStore({
     },
     state() {
         return {
+            // esto significa que somos el coach 'c3' que estamos esperando sus requests 
             userId: 'c3'
         };
     },
