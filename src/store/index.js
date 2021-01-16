f2e8b8e2ef16d3c70373c7a84a424af142b2f215
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-15 21:13:50 -0400

    Creando modulo auth
    - Creamos la estructura de carpetas del modulo auth.
    - Creamos la logica de archivos en todos los archivos

--- src/store/index.js
+++ src/store/index.js
@@ -2,23 +2,19 @@ import {createStore} from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestModule from './modules/requests/index.js';
-
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
-        requests: requestModule
+        requests: requestModule,
+        auth: authModule
     },
     state() {
         return {
             // esto significa que somos el coach 'c3' que estamos esperando sus requests 
             userId: 'c3'
         };
-    },
-    getters: {
-        userId(state){
-            return state.userId;
-        }
     }
 });
 
