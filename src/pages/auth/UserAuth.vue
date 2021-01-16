b90825a9a4338732a03cac6a0dda281278140548
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-15 22:37:26 -0400

    Agregando animaciones de error
    - Se modifico UserAuth.vue para agregar base-dialog de errores.
    - se modifico el script de UserAuth.vue para desplegar errores de acuerdo a los tipos que se presenten.

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,24 +1,32 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!isFormValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long).
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        switchModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!isFormValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -29,6 +37,8 @@ export default {
       password: '',
       isFormValid: true,
       mode: 'login',
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
@@ -48,8 +58,10 @@ export default {
     },
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.isFormValid = true;
+
+      // Validation.
       if (
         this.email === '' ||
         !this.email.includes('@') ||
@@ -59,15 +71,23 @@ export default {
         return;
       }
 
-      if(this.mode === 'login'){
+      this.isLoading = true;
+
+      try {
+        if (this.mode === 'login') {
           // ...
-      } else {
+        } else {
           // es directo a signup porque no tenemos nigun namespace.
-          this.$store.dispatch('signup', {
-              email: this.email,
-              password: this.password
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
           });
+        }
+      } catch (error) {
+        this.error = error.message || 'Failed to authenticate, try later.';
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
       if (this.mode === 'login') {
@@ -76,6 +96,9 @@ export default {
         this.mode = 'login';
       }
     },
+    handleError() {
+        this.error = null;
+    }
   },
 };
 </script>
